53b7ccd9d329f35710107c51f3ad75556aaa8ae1
adibsadeghi <adibsadeghi7272@gmail.com>
2021-04-05 12:07:32 +0430

    fetching products and show them

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,17 +1,28 @@
-
-import React from 'react'
-import Product from './Product'
-
+import axios from "axios";
+import React, { useEffect } from "react";
+import Product from "./Product";
+import { useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/ProductActions";
 
 const ProductList = () => {
-   
-    return (
-        <div className="product-list row">
-            <Product/>
-            
-            
-        </div>
-    )
-}
+  const dispatch = useDispatch();
 
-export default ProductList
+  const fetchProducts = () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => dispatch(setProducts(res.data)))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="ui stackable grid ">
+      <Product />
+    </div>
+  );
+};
+
+export default ProductList;
